31acb88b414ec221fea4730f8cf12bf7689b2dbe
Dylan Armitage <dylanjarmitage@gmail.com>
2023-01-03 19:16:31 -0800

    Black remaining files

--- model.py
+++ model.py
@@ -12,6 +12,7 @@ class Color(Enum):
     """
     Probably most hacky part of this. Logic is that the colors are represented by images with the given index
     """
+
     BLUE = 1
     RED = 2
     YELLOW = 3
@@ -30,27 +31,28 @@ class Highlight:
 
     @property
     def markdown_link(self):
-        return f'[{self.book}: {self.page}]({self.link})'
+        return f"[{self.book}: {self.page}]({self.link})"
 
     @property
     def color_attribute(self):
-        return f'color::#{self.color.name.lower()}'
+        return f"color::#{self.color.name.lower()}"
 
     @property
     def enhanced_color_attribute(self):
-        return f'color:: {self.color.name.lower()}'
+        return f"color:: {self.color.name.lower()}"
 
     @property
     def date_attribute(self):
-        return f'date::[[{roam_date(self.date)}]]'
+        return f"date::[[{roam_date(self.date)}]]"
 
     @property
     def enhanced_date_attribute(self):
-        return f'date:: {roam_date(self.date)}'
+        return f"date:: {roam_date(self.date)}"
 
     def as_roam_block_hierarchy(self):
         return {
-            self.text: ([{self.note: []}] if self.note else []) + [
+            self.text: ([{self.note: []}] if self.note else [])
+            + [
                 {self.markdown_link: []},
                 {self.date_attribute: []},
                 {self.color_attribute: []},
@@ -58,26 +60,34 @@ class Highlight:
         }
 
     def as_roam_markdown(self):
-        return seq(
-            f' - {self.text}',
-            f'   - {self.note}' if self.note else None,
-            f'   - {self.markdown_link}',
-            f'   - {self.date_attribute}',
-            f'   - {self.color_attribute}'
-        ).filter(lambda it: it is not None).make_string('\n')
+        return (
+            seq(
+                f" - {self.text}",
+                f"   - {self.note}" if self.note else None,
+                f"   - {self.markdown_link}",
+                f"   - {self.date_attribute}",
+                f"   - {self.color_attribute}",
+            )
+            .filter(lambda it: it is not None)
+            .make_string("\n")
+        )
 
     def as_enhanced_markdown(self):
-        return seq(
-            f'> [!quote]',
-            f'> text:: {self.text}',
-            f'> - {self.note}' if self.note else None,
-            f'> - {self.markdown_link}',
-            f'> - {self.enhanced_date_attribute}',
-            f'> - {self.enhanced_color_attribute}'
-        ).filter(lambda it: it is not None).make_string('\n')
+        return (
+            seq(
+                f"> [!quote]",
+                f"> text:: {self.text}",
+                f"> - {self.note}" if self.note else None,
+                f"> - {self.markdown_link}",
+                f"> - {self.enhanced_date_attribute}",
+                f"> - {self.enhanced_color_attribute}",
+            )
+            .filter(lambda it: it is not None)
+            .make_string("\n")
+        )
 
     def as_anki_csv_row(self):
         soup = BeautifulSoup()
-        link = soup.new_tag('a', href=self.link, string=f'{self.book}: {self.page}')
+        link = soup.new_tag("a", href=self.link, string=f"{self.book}: {self.page}")
 
         return [self.text, self.note, link, str(self.date), self.color.name.lower()]
--- roam.py
+++ roam.py
@@ -18,7 +18,7 @@ class Page:
 
     @classmethod
     def from_pull_result(cls, result: dict):
-        return cls(result['block/uid'], result['node/title'])
+        return cls(result["block/uid"], result["node/title"])
 
 
 @dataclass
@@ -28,11 +28,11 @@ class Block:
 
     @classmethod
     def from_pull_result(cls, result: dict):
-        return Block(result['block/uid'], result['block/string'])
+        return Block(result["block/uid"], result["block/string"])
 
     @classmethod
     def from_create_result(cls, result: dict):
-        return Block(result['uid'], result['string'])
+        return Block(result["uid"], result["string"])
 
 
 class RoamError(RuntimeError):
@@ -44,57 +44,64 @@ class RoamError(RuntimeError):
 
 
 def uid_param(uid):
-    return [('uid', uid)] if uid else []
+    return [("uid", uid)] if uid else []
 
 
 class Roam:
-    def __init__(self,
-                 graph_name: str,
-                 key: str,
-                 token: str,
-                 endpoint: str = 'https://4c67k7zc26.execute-api.us-west-2.amazonaws.com/v1/alphaAPI'):
+    def __init__(
+        self,
+        graph_name: str,
+        key: str,
+        token: str,
+        endpoint: str = "https://4c67k7zc26.execute-api.us-west-2.amazonaws.com/v1/alphaAPI",
+    ):
         self.graph_name = graph_name
         self.key = key
         self.token = token
         self.endpoint = endpoint
 
     def _send_request(self, action: str, **params):
-        payload = dict([('graph-name', self.graph_name)], action=action, **params)
-        log.info(f'Sending request to {self.endpoint}. For graph {self.graph_name} \n'
-                 f'With payload: {payload}')
+        payload = dict([("graph-name", self.graph_name)], action=action, **params)
+        log.info(
+            f"Sending request to {self.endpoint}. For graph {self.graph_name} \n"
+            f"With payload: {payload}"
+        )
 
-        response = requests.post(self.endpoint,
-                                 headers={
-                                     'x-api-key': self.key,
-                                     'x-api-token': self.token,
-                                 },
-                                 json=payload
-                                 )
+        response = requests.post(
+            self.endpoint,
+            headers={
+                "x-api-key": self.key,
+                "x-api-token": self.token,
+            },
+            json=payload,
+        )
         log.info(response)
         log.info(response.text)
         self.raise_errors(response)
-        result = json.loads(response.text)['success']
+        result = json.loads(response.text)["success"]
         return result
 
     def query(self, query: str) -> List:
-        return seq(self._send_request('q', query=query)).map(lambda it: it[0]).to_list()
+        return seq(self._send_request("q", query=query)).map(lambda it: it[0]).to_list()
 
     def pull(self, selector, uid):
-        return self._send_request('pull', selector=selector, uid=uid)
+        return self._send_request("pull", selector=selector, uid=uid)
 
     def get_page_by_title(self, title: str) -> Page:
-        result = self.query(f'[:find (pull ?page [*]) '
-                            f':where [?page :node/title "{title}"]'
-                            f']')
+        result = self.query(
+            f"[:find (pull ?page [*]) " f':where [?page :node/title "{title}"]' f"]"
+        )
 
         return Page.from_pull_result(result[0])
 
     def get_children(self, uid):
-        results = self.query('[:find (pull ?children [*])'
-                             ':where '
-                             f'[?block :block/uid "{uid}"]'
-                             '[?block :block/children ?children]'
-                             ']')
+        results = self.query(
+            "[:find (pull ?children [*])"
+            ":where "
+            f'[?block :block/uid "{uid}"]'
+            "[?block :block/children ?children]"
+            "]"
+        )
 
         return [Block.from_pull_result(block) for block in results]
 
@@ -103,36 +110,52 @@ class Roam:
         return seq(children).filter(lambda it: it.string == string).to_list()
 
     def get_all_blocks(self):
-        return self.query('[ :find (pull ?block [:block/string :block/uid]) :where [?block :block/string]]')
+        return self.query(
+            "[ :find (pull ?block [:block/string :block/uid]) :where [?block :block/string]]"
+        )
 
     def create_page(self, title: str, uid: Optional[str] = None):
-        result = self._send_request('create-page',
-                                    page=dict([('title', title)] + uid_param(uid)))
+        result = self._send_request(
+            "create-page", page=dict([("title", title)] + uid_param(uid))
+        )
         return from_dict(Page, result[0])
 
     @staticmethod
     def raise_errors(response):
-        error = json.loads(response.text).get('error')
+        error = json.loads(response.text).get("error")
         if error:
-            raise RoamError(error['cognitect.anomalies/message'],
-                            error['cognitect.anomalies/category'])
+            raise RoamError(
+                error["cognitect.anomalies/message"],
+                error["cognitect.anomalies/category"],
+            )
 
     def create_block(self, parent_uid: str, block: dict, order: int = 0):
         # todo uid support?
         string = next(iter(block))
-        response = self._send_request('create-block',
-                                      location={'parent-uid': parent_uid, 'order': order},
-                                      block={'string': string})
+        response = self._send_request(
+            "create-block",
+            location={"parent-uid": parent_uid, "order": order},
+            block={"string": string},
+        )
 
         result = Block.from_create_result(response[0])
-        return result, {result.uid: [self.create_block(result.uid, child) for child in reversed(block[string])]}
+        return result, {
+            result.uid: [
+                self.create_block(result.uid, child)
+                for child in reversed(block[string])
+            ]
+        }
 
 
 def strftime(date_format, date_to_format: date):
     def suffix(day):
-        return 'th' if 11 <= day <= 13 else {1: 'st', 2: 'nd', 3: 'rd'}.get(day % 10, 'th')
+        return (
+            "th" if 11 <= day <= 13 else {1: "st", 2: "nd", 3: "rd"}.get(day % 10, "th")
+        )
 
-    return date_to_format.strftime(date_format).replace('{S}', str(date_to_format.day) + suffix(date_to_format.day))
+    return date_to_format.strftime(date_format).replace(
+        "{S}", str(date_to_format.day) + suffix(date_to_format.day)
+    )
 
 
 def roam_date(date_to_format: date):
